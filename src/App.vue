2124a9ddba649def441e05631f8af9e125ffd048
codermomin1 <momingithub@gmail.com>
2023-07-25 14:27:22 +0600

    Style Binding

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,45 @@
 <script setup>
-const bgOrange = "bg-orange-400";
-const bgIndigo = "bg-indigo-400";
-const bgGreen = "bg-green-400";
+const bgOrange = 'background-color: rgb(251 146 60)'
+const bgBlue = 'background-color: rgb(96 165 250)'
+const bgGreen = 'background-color: rgb(74 222 128)'
 
-const bgRed = "bg-red-600";
-const bgGreen2 = "bg-green-500";
+const bgGreen2 = 'background-color: rgb(20 83 45)'
+const bgRed = 'background-color: rgb(127 29 29)'
 
-const weekend = false;
+const weekday = true
 
-const color = {
-  bgColor: "bg-teal-100",
-  borderColor: "border-teal-500",
-  textColor: "text-teal-900"
+const colors = {
+  backgroundColor: 'rgb(254 202 202)',
+  borderColor: 'rgb(185 28 28)',
+  color: 'color: rgb(127 29 29)'
 }
 
-const messageType = "error"
+const messageType = "success"
 
-if('warning' === messageType){
-  color.bgColor = "bg-orange-100",
-  color.borderColor = "border-orange-500"
-  color.textColor = "text-orange-900"
-}
-
-if('error' === messageType){
-  color.bgColor = "bg-red-100",
-  color.borderColor = "border-red-500"
-  color.textColor = "text-red-900"
+if('success' === messageType) {
+  colors.backgroundColor = 'rgba(204, 251, 241, 1)',
+  colors.color = 'rgba(19, 78, 74, 1)',
+  colors.borderColor= 'rgba(20, 184, 166, 1)'
 }
 </script>
 
 <template>
-  <section class="container mx-auto">
+  <section class="max-width: 1280px mx-auto">
     <h1 class="text-center text-2xl py-10">Class & Style Binding</h1>
-
     <section class="flex space-x-10">
-      <div class="w-1/3 h-16" :class="bgOrange"></div>
-      <div class="w-1/3 h-16" :class="bgIndigo"></div>
-      <div class="w-1/3 h-16" :class="bgGreen"></div>
+      <div class="w-1/3 h-16" :style="bgOrange"></div>
+      <div class="w-1/3 h-16" :style="bgBlue"></div>
+      <div class="w-1/3 h-16" :style="bgGreen"></div>
     </section>
 
     <section class="mt-10">
-      <div
-        class="h-16 flex items-center justify-center text-white"
-        :class="weekend == false ? bgGreen2 : bgRed"
-      >
-        {{ weekend === false ? "It's a weekday!" : "It's a weekend!" }}
+      <div class="h-16 flex justify-center text-white items-center" :style="weekday===true ? bgGreen2 : bgRed">
+      {{ weekday===true ? "It's Weekday Day" : "It's Weekend Day" }}
       </div>
     </section>
 
     <section class="mt-10">
-      <div
-        class="border-t-4 rounded-b px-4 py-3 shadow-md" :class="[color.bgColor, color.borderColor, color.textColor]" 
-      >
+      <div class=" border-t-4 px-4  h-16 rounded-b shadow-md" :style="colors">
         <p class="font-bold">Our privacy policy has changed</p>
         <p class="text-sm">Make sure you know how these changes affect you.</p>
       </div>
